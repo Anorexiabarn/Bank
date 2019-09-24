81e810de2ac9b7d2cd3b44bcd420f14b9f090254
Anorexiabarn <patrickjames.dougherty@elev.ga.ntig.se>
2019-09-24 11:03:28 +0200

    skapade en bank där man kan lägga till och/eller ta ut pengar osv

---
+++ PebbleBank.py
@@ -0,0 +1,40 @@
+# Start
+print("Välkommen till Pebbles Bank")
+# Login Details
+pin = 1221
+userPin = int(input("Skriv in din pinkod: "))
+if pin != userPin:
+    exit()
+
+# Saldo
+saldo = 0.0
+
+# Menu
+menu = 0
+
+menuList = ("1.insättning 2.uttag 3. logga ut 4. valuta: ")
+print(menuList)
+# Menu 1 insättning
+# Menu 2 uttag
+# Menu 3 valuta
+# Menu 4 logga ut
+while menu != 3:
+    menu = int(input("Skriv ditt val: "))
+    if menu == 1:
+        insättning = float(input("Hur mycket vill du lägga in?: "))
+        saldo = saldo + insättning
+        print(saldo)
+    elif menu == 2:
+        uttag = float(input("Hur mycket vill du ta ut? "))
+        saldo = saldo - uttag
+        print(saldo)
+        if saldo <= 0:
+            print("Saik! due fattig")
+    elif menu == 3:
+        print("loggar ut")
+        exit()
+    elif menu == 4:
+        print(saldo)
+
+    else:
+        print("Fel/avsluta")

06d91e2f0eaa774feb784faef62b3bce416428ac
Anorexiabarn <patrickjames.dougherty@elev.ga.ntig.se>
2019-10-09 12:54:45 +0200

    gjorde om några kommandon och gjorde så det såg bättre ut

--- PebbleBank.py
+++ PebbleBank.py
@@ -3,55 +3,53 @@ print("Välkommen till Pebbles Bank") # Printar ut början av programmet
 # Login Details
 pin = 1221 # En vaiabel som är pinkodet til programmet.
 userPin = int(input("Skriv in din pinkod: ")) # Det här variabeln gör så man måste skriva in pin och om det är fel så stängs programmet av.
-if pin != userPin:
+if pin != userPin: # detta gör så om man skriver fel pinkod så stängs programmet av.
     exit()
 
 # Saldo
 saldo = 0.0 # En variabel som visar start saldot
 
-# skapa en text fil som sparar saldo
-
-file = open("money.txt", 'r')
-money = file.readline()
+print("Här är ditt hela saldo")
+# sparar saldo till fil
+file = open("money.txt", 'r+')
+money = file.read()
 file.close()
-
+file = open("money.txt", "w")
+print(money)
 # Menu
 menu = 0 # En variabel
 
-menuList = ("1.insättning 2.uttag 3. logga ut 4. valuta: ") # En variabel som är en menylista och ordern man vill det ska vara.
-print(menuList) # printar ut menuList
+menuList = ("Insättning(1), Uttag(2), Kommer snart(3), Avsluta(4) ") # En variabel som är en menylista och ordern man vill det ska vara.
 # Menu 1 insättning
 # Menu 2 uttag
-# Menu 3 valuta
-# Menu 4 logga ut
-while menu != 3: # Detta gör så att sålänge man inte skriver 3 så loopar programmet om och om igen
-    menu = int(input("Skriv ditt val: "))
-    if menu == 1: # Detta variabeln är en if och det är "om" så om menu == 1 så går den till insättning till saldot.
-        insättning = float(input("Hur mycket vill du lägga in?: ")) # Detta har en float som är för heltal+decimaler och så ska man skriva in de tal man vill lägga in.
-        saldo = saldo + insättning # En variabel som är saldot + de insättning man lägger till.
-        print(saldo) # printar saldot.
+# Menu 3 övrigt
+# Menu 4 avsluta
+while menu == 0:
+    menu = int(input("Insättning [1], uttag [2], övrigt [3], avsluta [4]: ")) # Vad du vill göra närdu loggat in, skriver du något annat än ett heltal så går den tillbaka med hjälp av Else satsen längre ned
+    if menu == 1: # Insättning
+        insättning = float(input("Hur mycket vill du sätta in? ")) # Insättningen är en float, vilket gör att du kan skriva decimaltal
+        saldo = saldo + insättning # plusar ihop ditt saldo med insättning
+        print(saldo) # Skriver ut ditt saldo
+        menu = 0 # går tillbaka så du kan välja meny
 
-        open("file", "w")
+    elif menu == 2: # Uttag
+        uttag = float(input("Hur mycket vill du ta ut? "))
+        saldo = saldo - uttag # tar ditt saldo - de du vill ta ut.
+
+        if saldo < 0: # Om du har negativt saldo får du en varning om att pengarna är slut
+            print("Yikes due fattig") # printar ut detta om du försöker ta ut mer än vad du har.
+            menu = 0
+        print(saldo) # printar ut saldo
+        menu = 0
+    
+    elif menu == 3: # Inte gjort något här än men här kan fler funktioner läggas in
+        print("Coming soon.. så testa någon annan meny")
+        menu = 0 # går tillbaka till menyn
+
+    elif menu == 4: # Stänger av
+        print("Hejdå, vi synes nästa gång du är på snabbare")
         file.write(str(saldo))
-        file.close()
-        
-    elif menu == 2: # Detta variabel har en elif som är "else/if" och då går programmet till menu 2
-        uttag = float(input("Hur mycket vill du ta ut? ")) # Detta variabel uttag gör så att du ska skriva hur mycket du vill ta ut.
-        saldo = saldo - uttag # En variabel som visar saldo - uttaget man gör
-        print(saldo) # printar ut saldot
-        if saldo <= 0: # Detta if sats gör så om man ska ta ut pengar och tar ut så man har mindre en 0 så går det inte.
-            print("Saik! due fattig") # printar ut detta om man försöker ta ut mer än vad man har.
-    elif menu == 3: # Det stänger av och breakar loopen.
-        print("loggar ut") # printar ut detta om man "loggar ut"
-        break # breakar loopen
-    elif menu == 4: # Det printar ut de saldot du har på kontot
-        print(saldo) # printar saldot.
+        exit() # stänger av programet
 
-    else: # Detta gör så om man inte väljer något av de menyvalerna så händer detta.
-        print("Fel/avsluta") # printar ut om man skriver in fel meny
-        break # breakar loopen
-
-
-#f = open("money.txt", 'a')
-#f.write(str(saldo))
-#f.close()
+    else:
+        menu = 0 #Går tillbaka till menyn om du har skrivit fel eller något liknande
